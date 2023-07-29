8c2f30dd02474b631d39a1860a3cb2ed6c9b0130
Pavel Mezencev <mezencev24@mail.ru>
2023-07-29 10:59:46 +0300

    21. add page loader and not found page

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -2,10 +2,11 @@ import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
 import { Page } from 'shared/ui/Page/Page';
+import { PageLoader } from 'widgets/PageLoader';
 
 export function AppRouter() {
   return (
-    <Suspense fallback="">
+    <Suspense fallback={<PageLoader />}>
       <Routes>
         {Object.values(routeConfig)
           .map((route) => (
--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,15 +1,18 @@
 import { RouteProps } from 'react-router-dom';
 import { AboutPage } from 'pages/AboutPage';
 import { MainPage } from 'pages/MainPage';
+import { NotFound } from 'pages/NotFound';
 
 export enum AppRoutes {
   MAIN = 'main',
   ABOUT = 'about',
+  NOT_FOUND = 'not_found',
 }
 
 export const AppPaths: Record<AppRoutes, string> = {
   [AppRoutes.MAIN]: '/',
   [AppRoutes.ABOUT]: '/about',
+  [AppRoutes.NOT_FOUND]: '*',
 };
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
@@ -20,5 +23,9 @@ export const routeConfig: Record<AppRoutes, RouteProps> = {
   [AppRoutes.ABOUT]: {
     path: AppPaths.about,
     element: <AboutPage />
+  },
+  [AppRoutes.NOT_FOUND]: {
+    path: AppPaths.not_found,
+    element: <NotFound />
   },
 };
