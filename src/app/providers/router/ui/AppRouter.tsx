547a19c283f60fb1604478ced50b88ff7028b6ca
Pavel Mezencev <mezencev24@mail.ru>
2023-08-13 18:55:04 +0300

    48. add rpotected routes

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,30 +1,31 @@
-import { getAuthData } from 'entities/User/model/selectors/getAuthData/getAuthData';
-import { Suspense, memo, useMemo } from 'react';
-import { useSelector } from 'react-redux';
+import { Suspense, memo } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
 import { Page } from 'shared/ui/Page/Page';
 import { PageLoader } from 'widgets/PageLoader';
+import { RequireAuth } from './RequireAuth';
 
 export const AppRouter = memo(() => {
-  const isAuth = useSelector(getAuthData);
-  const routes = useMemo(
-    () => Object.values(routeConfig).filter((route) => {
-      if (!isAuth && route.auth) {
-        return false;
-      }
-      return true;
-    }),
-    [isAuth]
-  );
+  const routes = Object.values(routeConfig).map((route) => (
+    <Route
+      key={route.path}
+      path={route.path}
+      element={(
+        <Page>
+          {
+            route.auth
+              ? <RequireAuth>{route.element}</RequireAuth>
+              : route.element
+          }
+        </Page>
+      )}
+    />
+  ));
 
   return (
     <Suspense fallback={<PageLoader />}>
       <Routes>
-        {routes
-          .map((route) => (
-            <Route key={route.path} path={route.path} element={<Page>{route.element}</Page>} />
-          ))}
+        {routes}
       </Routes>
     </Suspense>
   );
